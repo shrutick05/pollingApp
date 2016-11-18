04088dac8c24a1a92b874c2c5545f2c1a1087677
Shruti Kale <shrutick05@gmail.com>
2016-11-18 09:58:37 +0530

    standard js

--- public/js/create.js
+++ public/js/create.js
@@ -1,80 +1,86 @@
-function addOption() {
-    let options = document.getElementById('options'),
-        id = options.children.length + 1,
-        html = ""
-    html += "<div id=\"" + id + "\" class=\"input-field col s6\">"
-    html += "<input id=\"option" + id + "\" class=\"validate\" type=\"text\" maxlength=\"150\" name=\"" + id + "\">"
-    html += "<label for=\"option\">Option " + id + "</label>"
-    html += "</div>"
-    options.insertAdjacentHTML('beforeend', html);
+function addOption () {
+  let options = document.getElementById('options')
+  let id = options.children.length + 1
+  let html = ''
+  html += '<div id=\'' + id + '\' class=\'input-field col s6\'>'
+  html += '<input id=\'option' + id + '\' class=\'validate\' type=\'text\' maxlength=\'150\' name=\'' + id + '\'>'
+  html += '<label for=\'option\'>Option ' + id + '</label>'
+  html += '</div>'
+  options.insertAdjacentHTML('beforeend', html)
 }
 
-function removeOption() {
-    let list = document.getElementById('options'),
-        item = list.lastElementChild
-    list.removeChild(item)
+function removeOption () {
+  let list = document.getElementById('options')
+  let item = list.lastElementChild
+  list.removeChild(item)
 }
 
-function checkSimilar() {
-    let options = document.getElementById('options').children,
-        count = options.length,
-        flag = true
-    let current = document.getElementById('option' + count).value.trim().toLowerCase()
-    while (count > 0) {
-        current = document.getElementById('option' + count).value.trim().toLowerCase()
-        for (let i = 1; i <= options.length; i++)
-            if (i !== count) {
-                let input = document.getElementById('option' + i).value.trim().toLowerCase()
-                if (input === current)
-                    flag = false
-            }
-        count--
-    }
-    return flag
+function checkSimilar () {
+  let options = document.getElementById('options').children
+  let count = options.length
+  let flag = true
+  let current = document.getElementById('option' + count).value.trim().toLowerCase()
+  while (count > 0) {
+      current = document.getElementById('option' + count).value.trim().toLowerCase()
+      for (let i = 1; i <= options.length; i++)
+          if (i !== count) {
+              let input = document.getElementById('option' + i).value.trim().toLowerCase()
+              if (input === current)
+                  flag = false
+          }
+      count--
+  }
+  return flag
 }
-
 
 let button = document.getElementById('form-button')
 IO.click(button)
   .then(() => {
     if (checkSimilar()) {
-        if (checkFilled())
-            document.getElementById('form-input').submit()
-        else
-            Materialize.toast("Don't leave any field empty", 2000)
-    } else
-        Materialize.toast("Two options can't be same", 3000)
+      if (checkFilled()) {
+        document.getElementById('form-input').submit()
+      } else {
+        Materialize.toast('Don\'t leave any field empty', 2000)
+      }
+    } else {
+        Materialize.toast('Two options can\'t be same', 3000)
+    }
   })
 
 let addOpt = document.getElementById('add-option')
 IO.click(addOpt)
   .then(() => {
-      if (checkSimilar()) {
-          if (checkFilled())
-              addOption()
-          else
-              Materialize.toast("Don't leave any field empty", 2000, 'rounded')
-      } else
-          Materialize.toast("Two options can't be same", 3000, 'rounded')
+    if (checkSimilar()) {
+      if (checkFilled()) {
+        addOption()
+      } else {
+        Materialize.toast("Don't leave any field empty", 2000, 'rounded')
+      }
+    } else {
+        Materialize.toast("Two options can't be same", 3000, 'rounded')
+    }
   })
 
 let removeOpt = document.getElementById('remove-option')
 IO.click(removeOpt)
   .then(() => {
     let options = document.getElementById('options').children.length
-        if (options > 2)
-            removeOption()
+    if (options > 2) {
+      removeOption()
+    }
   })
 
-function checkFilled() {
-    let options = document.getElementById('options').children.length,
-        flag = true
-    if (document.getElementById('question').value.length > 0) {
-        for (let i = 1; i <= options; i++) {
-            if (document.getElementById("option" + i).value.length <= 0)
-                flag = false
-        }
-    } else
+function checkFilled () {
+  let options = document.getElementById('options').children.length
+  let flag = true
+  if (document.getElementById('question').value.length > 0) {
+    for (let i = 1; i <= options; i++) {
+      if (document.getElementById('option' + i).value.length <= 0) {
         flag = false
-    return flag
+      }
+    }
+  } else {
+    flag = false
+  }
+  return flag
 }
